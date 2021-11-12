c3ecae3a2a9ea0f889bdd383838cc6d89c4b8c00
TheAverageStudents <70644006+TheAverageStudents@users.noreply.github.com>
2021-11-12 14:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+//console.log(document.styleSheets.item(0).cssRules)
+
+/*var s = document.getElementsByClassName("stylesheet")[0]
+s.innerHTML = String(`<style>
+    ${fetch("https://localhost:8080/style.css")}
+</style>`)*/
+
+//document.createElement("style").innerHTML = fetch()
+/*
+var doc = document.getElementById("document")
+doc.innerHTML = String(doc.innerHTML + `
+<style>
+    #topbar{
+        position: fixed;
+        height: 80px;
+        width: 100%;
+        background-color: grey;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 30px;
+        padding-left: 1%;
+        margin-left: -1%;
+    }
+
+    #ThemeButton{
+        height: 40px;
+        width: 8%;
+        margin-top: 20px;
+        margin-left: 70%;
+        border-radius: 10px;
+    }
+
+    #coverup{
+        height: 80px;
+        width: 100%;
+        background-color: grey;
+        font-size: 30px;
+        margin-top: -1%;
+    }
+
+
+    #AboutMe{
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
+
+    #Part1{
+        margin-top: 2%;
+        font-size: 35px;
+        width: 100%;
+    }
+
+    #Picture1{
+        width: 15%;
+    }
+
+    #Text1{
+        margin-left: 30%;
+        font-size: 400%;
+    }
+
+    #ColorText1{
+        color: red;
+    }
+
+    #ColorText2{
+        color: chartreuse;
+    }
+
+    #ColorText3{
+        color: yellow;
+    }
+
+    #ColorText4{
+        color: orangered;
+    }
+
+    #ColorText5{
+        color: blue;
+    }
+
+    #ColorText6{
+        color: yellowgreen;
+    }
+
+    #ColorText7{
+        color: darkblue;
+    }
+
+    #ColorText8{
+        color: #5865F2;
+    }
+
+    #Picture2{
+        width: 15%;
+        margin-left: 85%;
+    }
+
+    #Sec2{
+        margin-top: 5%;
+    }
+
+    #Text2{
+        margin-left: 45%;
+        font-size: 35px;
+    }
+
+    #Text3{
+        font-size: 35px;
+    }
+
+    #Text4{
+        font-size: 35px;
+    }
+
+    html{
+        height: 100%;
+        width: 100%;
+        margin-left: -0.4%;
+    }
+</style>`)
+*/
+
